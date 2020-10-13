4e51159d9686a4fb198151385741a21384e7aa72
Jacob Turner <jacob11turner@gmail.com>
2020-10-13 13:05:40 +0100

    Add libgdx template and VSCode tools

---
+++ desktop/src/com/dragonboatrace/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dragonboatrace.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dragonboatrace.game.DragonBoatRace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DragonBoatRace(), config);
+	}
+}
