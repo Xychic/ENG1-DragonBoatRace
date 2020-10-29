f539d1300ceaa45a887521e3249696204df074e0
Jacob Turner <jacob11turner@gmail.com>
2020-10-29 14:43:11 +0000

    Merge changes

--- core/src/com/dragonboatrace/game/ObstacleType.java
+++ core/src/com/dragonboatrace/game/ObstacleType.java
@@ -4,14 +4,20 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
 public enum ObstacleType {
-    //ID, Weight, Size, imageSrc, mover
-    ROCK("rock", 1, new Vector2(1,1), null, MovementCharacteristics.STATIC);
+    //      ID,             Weight, Size,                   imageSrc,   mover
+    ROCK(   "rock",         4,      new Vector2(1,1),       null,       MovementCharacteristics.STATIC),
+    BRANCH( "branch" ,      1,      new Vector2(1,1),       null,       MovementCharacteristics.STATIC),
+    DUCK(   "duck",         2,      new Vector2(1,1) ,      null,       MovementCharacteristics.STATIC),
+    LONGBOI("longboi",      2,      new Vector2(1,1),       null,       MovementCharacteristics.STATIC),
+    BOAT(   "boat",         5,      new Vector2(1,1),       null,       MovementCharacteristics.STATIC),
+    RUBBISH("rubbish" ,     1,      new Vector2(1,1),       null,       MovementCharacteristics.STATIC),
+    BUOY(   "buoy",         3,      new Vector2(1,1),       null,       MovementCharacteristics.STATIC);
 
     String ID;
     float weight;
     Vector2 size;
     Texture image;
-    MovementCharacteristics mover; 
+    MovementCharacteristics mover;
 
     private ObstacleType(String ID, float weight, Vector2 size, String imageSrc, MovementCharacteristics mover){
         this.ID = ID;
