dd35c243094418aadd072704db2f8651686aa83b
Jacob Turner <jacob11turner@gmail.com>
2020-10-29 15:00:06 +0000

    Merge changes

--- core/src/com/dragonboatrace/game/DragonBoatRace.java
+++ core/src/com/dragonboatrace/game/DragonBoatRace.java
@@ -5,29 +5,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class DragonBoatRace extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	PlayerBoat pb;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		pb = new PlayerBoat(BoatType.NORMAL, new Vector2(0, 0));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		pb.render(batch);
+		pb.move((float)10);
+		pb.update((float) 1);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		pb.dispose();
 	}
 }
